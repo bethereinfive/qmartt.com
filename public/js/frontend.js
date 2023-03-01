9a4fe0af140a9a3c393fc7bbe35c1f85bb821098
Softlabltd.com <freelancernishad123@gmail.com>
2023-03-01 12:00:43 +0600

    update

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -2493,7 +2493,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
-  props: ['user'],
+  //  props: ['user'],
   created: function created() {
     if (User.loggedIn()) {
       var localRole = localStorage.getItem('role');
--- resources/js/frontend/components/layouts/layout.vue
+++ resources/js/frontend/components/layouts/layout.vue
@@ -5,7 +5,7 @@
 </template>
 <script>
 export default {
-     props: ['user'],
+    //  props: ['user'],
     created() {
 
 
